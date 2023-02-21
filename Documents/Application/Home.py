0f5ac2efd1ff6094e07ba7639ac8adccbb50a75b
DEVIDHARSANA19 <dharsana.619@gmail.com>
2023-02-21 17:41:53 +0530

    first commit

---
+++ Documents/Application/Home.py
@@ -0,0 +1,10 @@
+#Title              :Online Cosmetic Shopping 
+#Author             :Devi Dharsana P
+#Created on         :09.02.2023
+#Last Modified Date :19.02.2023
+#Reviewed by        :Silpa
+#Reviewed on        :20.02.2023
+from Login import User 
+User().home()
+
+
